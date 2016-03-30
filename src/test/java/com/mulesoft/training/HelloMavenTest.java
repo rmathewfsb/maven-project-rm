f047fc6e99db63aa179fa6732713362415fcd24e
rmathewfsb <roy.mathew@flagstar.com>
2016-03-30 12:28:55 -0400

    Added a failing test to watch jenkins test results

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -3,6 +3,7 @@ package com.mulesoft.training;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 
 public class HelloMavenTest extends FunctionalTestCase {
@@ -15,6 +16,13 @@ public class HelloMavenTest extends FunctionalTestCase {
     @Override
     protected String getConfigFile() {
         return "maven-project-rm.xml";
+    }
+    
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+      MuleEvent event = runFlow("retrieveFlights");
+      String contentType = event.getMessage().getOutboundProperty("Content-Type");
+      assertEquals("application/json", contentType);
     }
 
 }
